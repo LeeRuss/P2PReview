4d17c40ad773ee25383e973c9f554b4b052294ed
LeeRuss <96124920+LeeRuss@users.noreply.github.com>
2023-11-23 16:55:20 +0100

    Added routes

---
+++ src/app/userWork/UserWork.jsx
@@ -0,0 +1,3 @@
+export default function UserWork() {
+  return <>User Work</>;
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import { Amplify } from 'aws-amplify';
 import awsExports from './aws-exports.js';
 import '@aws-amplify/ui-react/styles.css';
+import App from './App.jsx';
+import Settings from './app/settings/Settings.jsx';
+import WorkExplorer from './app/workExplorer/WorkExplorer.jsx';
+import UserWork from './app/userWork/UserWork.jsx';
+import UserReviews from './app/userReviews/UserReviews.jsx';
+
 Amplify.configure(awsExports);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route index path="/" element={<App />}></Route>
+        <Route path="/" element={<App />}>
+          <Route index element={<WorkExplorer />}></Route>
+          <Route index path="/yourWork" element={<UserWork />}></Route>
+          <Route index path="/yourReviews" element={<UserReviews />}></Route>
+          <Route index path="/settings" element={<Settings />}></Route>
+        </Route>
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
