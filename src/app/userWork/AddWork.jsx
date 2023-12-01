707cecd8e52ea6b9fdfa2c4308f6a8402e86320d
LeeRuss <96124920+LeeRuss@users.noreply.github.com>
2023-12-01 16:30:06 +0100

    Added form for work creating
    work need to have:
    -title
    -short description
    -full description
    -department
    -advancement level
    and can have
    links and their description

--- src/app/userWork/AddWork.jsx
+++ src/app/userWork/AddWork.jsx
@@ -1,5 +1,50 @@
 import { Container, Paper, Typography, Divider } from '@mui/material';
+import AddWorkForm from './AddWorkForm';
 
 export default function AddWork() {
-  return <></>;
+  return (
+    <Container
+      maxWidth="xl"
+      sx={{
+        minHeight: '80vh',
+        maxHeight: '80vh',
+        minWidth: { xs: '90vw', md: '65vw', lg: '40vw' },
+        maxWidth: { xs: '90vw', md: '65vw', lg: '40vw' },
+        padding: '0',
+        display: 'flex',
+        alignItems: 'stretch',
+        marginTop: { xs: '12.5vh', md: '10vh', lg: '10vh' },
+        marginBottom: '1rem',
+      }}
+    >
+      <Paper elevation={8} sx={{ minHeight: '100%', width: '100%' }}>
+        <Container
+          maxWidth="xl"
+          sx={{
+            height: '100%',
+            width: '100%',
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            padding: '1rem',
+            marginLeft: 0,
+            marginRight: 0,
+            border: 1,
+            borderColor: 'rgb(128, 128, 128)',
+            borderRadius: 1,
+          }}
+        >
+          <Typography component="h1" variant="h3">
+            Create work to review
+          </Typography>
+          <Divider
+            variant="middle"
+            flexItem
+            sx={{ mt: '0.5rem', mb: '1rem', borderWidth: '1.5px' }}
+          />
+          <AddWorkForm></AddWorkForm>
+        </Container>
+      </Paper>
+    </Container>
+  );
 }
