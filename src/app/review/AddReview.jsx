a3345998c923ec05873826db60c8a8eeaee4bd28
LeeRuss <96124920+LeeRuss@users.noreply.github.com>
2024-01-07 11:57:25 +0100

    Added dialog to AddReview component

--- src/app/review/AddReview.jsx
+++ src/app/review/AddReview.jsx
@@ -1,13 +1,43 @@
-import { Button } from '@mui/material';
+import { useState } from 'react';
+import {
+  Button,
+  Dialog,
+  Box,
+  DialogTitle,
+  DialogActions,
+  DialogContent,
+} from '@mui/material';
 import { useParams } from 'react-router-dom';
 
 export default function AddReview() {
   const { workId } = useParams();
+  const [dialogOpen, setDialogOpen] = useState(false);
+
+  function onSubmit() {}
+
+  function handleClose() {
+    setDialogOpen(false);
+  }
+
   return (
     <>
-      <Button variant="contained" sx={{ minWidth: '25%', maxWidth: '80%' }}>
-        Review / {workId}
+      <Button
+        onClick={() => setDialogOpen(true)}
+        variant="contained"
+        sx={{ minWidth: '25%', maxWidth: '80%' }}
+      >
+        Review
       </Button>
+      <Dialog open={dialogOpen} onClose={handleClose}>
+        <Box component="form" onSubmit={onSubmit} noValidate>
+          <DialogTitle>Review current work</DialogTitle>
+          <DialogContent></DialogContent>
+          <DialogActions>
+            <Button onClick={handleClose}>Cancel</Button>
+            <Button type="submit">Upload review</Button>
+          </DialogActions>
+        </Box>
+      </Dialog>
     </>
   );
 }
